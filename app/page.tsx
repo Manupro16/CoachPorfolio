6dae7d092f96eb2bc96b2b40324f08e1359893a8
Manupro16 <vera.manuel2004@gmail.com>
2024-09-16 11:07:09 -0500

    Upgrade dependencies in package-lock.json
    Updated various dependencies including major libraries such as Next.js, React, and Tailwind CSS. Additionally, several new utility and plugin packages were added to enhance project functionalities.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import WelcomeSection from "@/app/WelcomeSection";
 import {Grid} from "@radix-ui/themes";
+import ShowcaseSection from "@/app/ShowcaseSection";
 
 function Home() {
     return (
-        <Grid as='div' columns='1' rows='1fr 1fr 1fr'>
-            <WelcomeSection />
-            <div>Content goes here</div>
+        <Grid as='div' columns='1' rows='1fr 1fr '>
+            <WelcomeSection/>
+            <ShowcaseSection/>
             <div>Footer goes here</div>
         </Grid>
     );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,61 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from 'tailwindcss/defaultTheme';
+import colors from 'tailwindcss/colors';
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    // Include other directories if necessary
   ],
+  darkMode: 'class', // Enables dark mode using the 'class' strategy
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        accent: "var(--accent)",
+        gray: {
+          ...colors.gray,
+          850: '#1a1a1a',
+        },
+      },
+      fontFamily: {
+        sans: ['var(--font-geist-sans)', ...defaultTheme.fontFamily.sans],
+        mono: ['var(--font-geist-mono)', ...defaultTheme.fontFamily.mono],
+      },
+      spacing: {
+        '18': '4.5rem',
+        '22': '5.5rem',
+      },
+      borderRadius: {
+        'xl': '1rem',
+        '2xl': '1.5rem',
+      },
+      boxShadow: {
+        'custom-light': '0 2px 4px rgba(255, 255, 255, 0.1)',
+        'custom-dark': '0 2px 4px rgba(0, 0, 0, 0.5)',
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+      },
+      zIndex: {
+        '60': '60',
+        '70': '70',
       },
     },
+    screens: {
+      xs: '480px',
+      ...defaultTheme.screens,
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
 export default config;
