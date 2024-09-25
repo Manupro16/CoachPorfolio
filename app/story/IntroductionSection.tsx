1e9d9cb505c2605c34cfe850d10e10eeb29ea507
Manupro16 <vera.manuel2004@gmail.com>
2024-09-25 13:51:00 -0500

    Add IntroductionSection component to streamline Intro content
    Refactor the introduction section to a standalone `IntroductionSection` component for better modularity and readability. Updated `StoryPage` to use the new component, reducing redundancy and improving maintainability.

---
+++ app/story/IntroductionSection.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Badge, Box, Flex, Heading, Quote, Text } from '@radix-ui/themes';
+
+interface IntroductionSectionProps {
+    playerStatus: string;
+    coachStatus: string;
+}
+
+const IntroductionSection: React.FC<IntroductionSectionProps> = ({
+                                                                     playerStatus,
+                                                                     coachStatus,
+                                                                 }) => {
+    return (
+        <Flex
+            direction="column"
+            align="center"
+            className="text-center mb-16 px-4 sm:px-6 lg:px-8"
+        >
+            <Heading
+                as="h1"
+                size="8"
+                weight="bold"
+                className="text-white leading-tight"
+                style={{ textShadow: '1px 1px 2px rgba(0, 0, 0, 0.7)' }}
+            >
+                Jose de Jesus Vera
+            </Heading>
+            <Box as="span" className="block h-[3px] w-1/3 bg-primary mt-2 mb-4" />
+            <Text
+                as="p"
+                size="4"
+                className="text-gray-200 max-w-2xl"
+                style={{ textShadow: '1px 1px 2px rgba(0, 0, 0, 0.5)' }}
+            >
+                The remarkable journey of Jose De Jesus Vera, most known as{' '}
+                <Quote>El Chuy Vera</Quote>, as a player and coach. In this section, we
+                highlight his time playing for and coaching various teams, tracing his
+                long journey from a young age to becoming a legendary athlete and coach.
+            </Text>
+            <Text as="p" size="3" className="text-gray-300 mt-2">
+                Player Career status: <Badge color="red">{playerStatus}</Badge>
+            </Text>
+            <Text as="p" size="3" className="text-gray-300 mt-2">
+                Coaching Career status: <Badge color="green">{coachStatus}</Badge>
+            </Text>
+        </Flex>
+    );
+};
+
+export default IntroductionSection;
--- app/story/page.tsx
+++ app/story/page.tsx
@@ -12,6 +12,7 @@ import {
     Text,
 } from '@radix-ui/themes';
 import Image from 'next/image';
+import IntroductionSection from "@/app/story/IntroductionSection";
 
 function StoryPage() {
     // Teams he played for
@@ -60,37 +61,8 @@ Duis facilisis tincidunt facilisis rhoncus proin. Eros odio egestas congue, tort
 
             {/* Main Content */}
             <Box className="relative z-10 px-4 sm:px-6 lg:px-8 py-5">
-                {/* Introduction */}
-                <Flex
-                    direction="column"
-                    align="center"
-                    className="text-center mb-16 px-4 sm:px-6 lg:px-8"
-                >
-                    <Heading
-                        as="h1"
-                        size="8"
-                        weight="bold"
-                        className="text-white leading-tight"
-                        style={{ textShadow: '1px 1px 2px rgba(0, 0, 0, 0.7)' }}
-                    >
-                        Jose de Jesus Vera
-                    </Heading>
-                    <Box as="span" className="block h-[3px] w-1/3 bg-primary mt-2 mb-4" />
-                    <Text
-                        as="p"
-                        size="4"
-                        className="text-gray-200 max-w-2xl"
-                        style={{ textShadow: '1px 1px 2px rgba(0, 0, 0, 0.5)' }}
-                    >
-                        The remarkable journey of Jose De Jesus Vera, most known as <Quote>El Chuy Vera</Quote>, as a player and coach. In this section, we highlight his time playing for and coaching various teams, tracing his long journey from a young age to becoming a legendary athlete and coach.
-                    </Text>
-                    <Text as="p" size="3" className="text-gray-300 mt-2">
-                        Player Career status: <Badge color="red">Retired</Badge>
-                    </Text>
-                    <Text as="p" size="3" className="text-gray-300 mt-2">
-                        Coaching Career status: <Badge color="green">Active</Badge>
-                    </Text>
-                </Flex>
+                <IntroductionSection playerStatus="Retired" coachStatus="Active" />
+
                 {/* Early Life */}
                 <Box className="relative w-full mb-16 pt-10 px-4 sm:px-6 lg:px-8">
                     <Grid
