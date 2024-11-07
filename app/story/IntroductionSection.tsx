7c6375ed88e9d380062b50f97ba1a4b5746f042f
Manupro16 <vera.manuel2004@gmail.com>
2024-11-06 22:40:01 -0600

    Enhance IntroductionSection with icons and responsive text size.
    Added `LightningBoltIcon` and `MagicWandIcon` to player and coach status badges for better visual context. Also updated responsive sizes for `Heading` and `Text` components along with minor style adjustments for improved user experience and readability.

--- app/story/IntroductionSection.tsx
+++ app/story/IntroductionSection.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Badge, Box , Flex, Heading, Quote, Text} from '@radix-ui/themes';
+import {Badge, Box, Flex, Heading, Quote, Text,} from '@radix-ui/themes';
+import {LightningBoltIcon, MagicWandIcon,} from '@radix-ui/react-icons'
 
 
 interface IntroductionSectionProps {
@@ -7,39 +8,52 @@ interface IntroductionSectionProps {
     coachStatus: string;
 }
 
-const IntroductionSection: React.FC<IntroductionSectionProps> = ({ playerStatus, coachStatus }) => {
+const IntroductionSection: React.FC<IntroductionSectionProps> = ({playerStatus, coachStatus}) => {
     return (
         <Flex
             as="div"
             direction="column"
             align="center"
+            py="4"
             className="text-center mb-16 px-4 sm:px-6 lg:px-8 relative"
         >
             <Heading
                 as="h1"
-                size="8"
+                size={{initial: '8', sm: '8', md: '8'}}
                 weight="bold"
                 className="text-textLight leading-tight "
-                style={{ textShadow: '1px 1px 2px rgba(0, 0, 0, 0.7)' }}
+                style={{textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)'}}
             >
                 Jose de Jesus Vera
             </Heading>
-            <Box as="span" className="block h-[3px] w-1/3 bg-primary mt-2 mb-4" />
+            <Box as="span" className="block h-[3px] w-1/3 bg-primary mt-2 mb-4"/>
             <Text as="p"
-                    size="4" className="text-gray-200 max-w-2xl"
-                    style={{textShadow: '1px 1px 2px rgba(0, 0, 0, 0.5)' }}
+                  size={{initial: '4', sm: '5'}}
+                  className="text-gray-200 max-w-2xl"
+                  style={{textShadow: '1px 1px 2px rgba(0, 0, 0, 0.5)'}}
             >
                 The remarkable journey of Jose De Jesus Vera, most known as{' '}
                 <Quote>El Chuy Vera</Quote>, as a player and coach. In this section, we
                 highlight his time playing for and coaching various teams, tracing his
                 long journey from a young age to becoming a legendary athlete and coach.
             </Text>
-            <Text as="p" size="3" className="text-gray-300 mt-2">
-                Player Career status: <Badge color="red">{playerStatus}</Badge>
-            </Text>
-            <Text as="p" size="3" className="text-gray-300 mt-2">
-                Coaching Career status: <Badge color="green">{coachStatus}</Badge>
-            </Text>
+            <Flex gap="4" className="mt-6 flex-wrap justify-center">
+                <Badge
+                    color="red"
+                    variant="surface"
+                    size="2"
+
+                >
+                    <LightningBoltIcon className="mr-2"/> Player Career: {playerStatus}
+                </Badge>
+                <Badge
+                    color="green"
+                    variant="surface"
+                    size="2"
+                >
+                    <MagicWandIcon className="mr-2"/> Coaching Career: {coachStatus}
+                </Badge>
+            </Flex>
         </Flex>
 
     );
