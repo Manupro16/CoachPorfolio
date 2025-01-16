d891a6d7cc9e6ae370af0d36ff53fd6e3be9e060
Manupro16 <vera.manuel2004@gmail.com>
2025-01-16 13:33:23 -0600

    Remove unnecessary height attributes from components
    Removed redundant height properties in Grid and section elements to simplify styling and ensure better responsiveness. The changes streamline the layout without altering the visual structure.

--- app/career/page.tsx
+++ app/career/page.tsx
@@ -5,16 +5,15 @@ import CareerProfile from "@/app/career/CareerProfile";
 
 function careerPageSection() {
     return (
-        <section style={{ width: "100vw", height: "100vh" }}>
+        <section style={{ width: "100vw" }}>
             <WebsiteBackgroundColor />
-            <Grid as="div" width="100%" height="auto" columns="1" rows={{ initial: "1fr 1fr 1fr" }}>
+            <Grid as="div" width="100%"  columns="1" rows={{ initial: "1fr 1fr 1fr" }}>
                  <CareerProfile/>
                 <div>Hello Section</div>
                 <div>Hello Section</div>
             </Grid>
         </section>
     )
-
 }
 
 export default careerPageSection;
